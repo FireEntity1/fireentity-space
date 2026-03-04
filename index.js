d0ee3a714fb8c4243806aded5b0698ad2a2bdcdf
fireentity <134467282+FireEntity1@users.noreply.github.com>
2026-03-04 12:42:37 -0700

    feat: completed site

---
+++ index.js
@@ -0,0 +1,38 @@
+var projects = {
+    "music": [
+        {
+            "name": "Unbound",
+            "description": "A song about breaking free from people's expectations and finding your own path. My first lyrical song!",
+            "link": "https://www.youtube.com/watch?v=lMCy1eDiFDw",
+            "image": "assets/kaguya.png"
+        }
+    ],
+    "code": [
+        {
+            "name": "Voidborne",
+            "description": "A story game where void enters a dystopian world of light.",
+            "link": "https://fire-entity.itch.io/voidborne",
+            "image": "assets/voidborne.png"
+        }
+    ],
+    
+}
+
+var selected = 0
+
+function update_content(index) {
+    var content = document.getElementById("content")
+    content.innerHTML = ""
+    var category = Object.keys(projects)[index]
+    projects[category].forEach(project => {
+        var project_element = document.createElement("div")
+        project_element.classList.add("bg-white/10", "rounded-lg", "p-5", "mb-5", "text-center", "font", "mt-2")
+        project_element.innerHTML = `
+            <h2 class="text-xl font-bold mb-2 font">${project.name}</h2>
+            <p class="mb-2 font">${project.description}</p>
+            <a href="${project.link}" class="text-blue-500 hover:underline">View Project</a>
+            <img src="${project.image}" alt="${project.name}" class="mt-2 rounded-lg mx-auto">
+        `
+        content.appendChild(project_element)
+    })
+}
